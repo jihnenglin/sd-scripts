69439444907195f243f1ae5ed1f174ff8ee98ad3
jihnenglin <ro12847@gmail.com>
2023-11-11 18:07:35 +0800

    Create LoRA_finetuner_testing.py

---
+++ LoRA_finetuner_testing.py
@@ -0,0 +1,71 @@
+## Visualize loss graph
+import os
+import subprocess
+
+root_dir = "~/sd-train"
+training_logs_path = os.path.join(root_dir, "LoRA/logs")
+
+repo_dir = os.path.join(root_dir, "sd-scripts")
+os.chdir(repo_dir)
+subprocess.run(f"tensorboard --logdir {training_logs_path}", shell=True)
+subprocess.run(f"ngrok http 6006", shell=True)
+
+input("Press the Enter key to continue: ")
+
+
+## Interrogating LoRA Weights
+# Now you can check if your LoRA trained properly.
+import torch
+import json
+from safetensors.torch import load_file
+from safetensors.torch import safe_open
+
+# If you used `clip_skip = 2` during training, the values of `lora_te_text_model_encoder_layers_11_*` will be `0.0`, this is normal. These layers are not trained at this value of `Clip Skip`.
+network_weight = ""
+verbose = False
+
+def is_safetensors(path):
+    return os.path.splitext(path)[1].lower() == ".safetensors"
+
+def load_weight_data(file_path):
+    if is_safetensors(file_path):
+        return load_file(file_path)
+    else:
+        return torch.load(file_path, map_location="cuda")
+
+def extract_lora_weights(weight_data):
+    lora_weights = [
+        (key, weight_data[key])
+        for key in weight_data.keys()
+        if "lora_up" in key or "lora_down" in key
+    ]
+    return lora_weights
+
+def print_lora_weight_stats(lora_weights):
+    print(f"Number of LoRA modules: {len(lora_weights)}")
+
+    for key, value in lora_weights:
+        value = value.to(torch.float32)
+        print(f"{key}, {torch.mean(torch.abs(value))}, {torch.min(torch.abs(value))}")
+
+def print_metadata(file_path):
+    if is_safetensors(file_path):
+        with safe_open(file_path, framework="pt") as f:
+            metadata = f.metadata()
+        if metadata is not None:
+            print(f"\nLoad metadata for: {file_path}")
+            print(json.dumps(metadata, indent=4))
+    else:
+        print("No metadata saved, your model is not in safetensors format")
+
+def main(file_path, verbose: bool):
+    weight_data = load_weight_data(file_path)
+
+    if verbose:
+        lora_weights = extract_lora_weights(weight_data)
+        print_lora_weight_stats(lora_weights)
+
+    print_metadata(file_path)
+
+if __name__ == "__main__":
+    main(network_weight, verbose)
