e37381b9c7b89a108b27ed425bcb731be7711804
jihnenglin <ro12847@gmail.com>
2023-12-11 16:09:31 +0800

    add merge json

---
+++ merge_json.py
@@ -0,0 +1,14 @@
+import json
+
+NUM_OF_DEVICES = 4
+
+json_path = "~/sd-train/json/meta_lat.json"
+
+merged_metadata = {}
+for i in range(NUM_OF_DEVICES):
+    with open(f"{json_path[:-5]}_{i}.json", "r") as f:
+        merged_metadata.update(json.load(f))
+
+print(f"metadata: {len(merged_metadata)} entries")
+with open(json_path, "w") as f:
+    json.dump(merged_metadata, f, indent=2)
